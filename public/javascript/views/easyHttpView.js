824012d66e71503670aac076583a62f15344aa07
oluleyepeters <oluleyepeters@gmail.com>
2019-10-22 10:19:54 -0700

    Https Corrected

--- public/javascript/views/easyHttpView.js
+++ public/javascript/views/easyHttpView.js
@@ -13,12 +13,14 @@ const clearResults = () =>{
 const displayResult = (posts) =>{
 	let allMovies = '';
 	posts.forEach((post) => {
-		allMovies +=`<div class="col-lg-3 col-md-6 col-sm-6 mb-2 mt-2 justify-content-center">
+		allMovies +=`<div class="col-lg-3 col-md-6 col-sm-6 mb-2 mt-3 justify-content-center">
 						<div class="mx-auto" style="height: 300px; width: 250px;">
 							<img class="img-fluid mb-1" style="height: 100%; width: 100%;" src=${post.Poster} />
 						</div>
-						<h4 class="mx-auto text-center">${post.Title}</h4>
-    					<a class="mx-auto text-center card-details details" href="#" data-goto=${post.imdbID}>View Details</a>
+						<h5 class="mx-auto text-center">${post.Title}</h5>
+    					<div class="d-flex" justify-content-between>
+							<a class="mx-auto text-center card-details details" href="#" data-goto=${post.imdbID}>View Details</a>
+    					</div>	
 					</div>`
 	})
 	elements.movieContainer.innerHTML = allMovies;
